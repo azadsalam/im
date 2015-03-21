d680e706eb93e5b98fa24b23977bec36924bf9fb
azadsalam <azadsalam2611@gmail.com>
2015-03-21 20:17:24 +0600

    scrap

--- application/controllers/items_summary.php
+++ application/controllers/items_summary.php
@@ -1,8 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Items_location extends CI_Controller {
+class Items_summary extends CI_Controller {
 
 	public $lname='';
+	public $type_id='';
 	public function __construct()
 	{
 		parent::__construct();
@@ -34,14 +35,19 @@ class Items_location extends CI_Controller {
 		redirect_if_not_logged_in();
 		
 		$this->load->helper('form');
-		$this->form_validation->set_rules('lname', 'Location', 'required|trim|xss_clean');						
+		$this->form_validation->set_rules('lname', 'Location', 'trim|xss_clean');				
+		$this->form_validation->set_rules('type_id','Type','trim|xss_clean');		
 		$this->form_validation->set_error_delimiters('<br /><span class="error">', '</span>');
 	
 		$this->lname = NULL;
+		$this->type_id = NULL;
 		if ($this->form_validation->run() == TRUE) // validation hasn't been passed
 		{
 			$this->lname = $this->input->post('lname');
+			$this->type_id = $this->input->post('type_id');
 		}
+		
+		//echo $this->lname . ' <-> '.$this->type_id;
 
 //		echo $this->lname;
 //		$this->load->view('items_location_view_part1',array('lname'=>$lname));
@@ -49,18 +55,33 @@ class Items_location extends CI_Controller {
 		$output=NULL;
 		try
 		{
-			if(isset($this->lname))
-			{
+//			if(isset($this->lname))
+//			{
 				$crud = new grocery_CRUD();
 				$crud->set_theme('datatables');
 				$crud->set_table('item');
 				
 				$crud->required_fields('type_id','name','lid');
 				
-				$this->load->model('location');
-				$lid = $this->location->get_location_id($this->lname);
-				//echo $lid;
-				$crud->where('lid',$lid);
+				
+				if(isset($this->lname) && !empty($this->lname))
+				{
+					if($this->lname == 'NO_LOCATION')
+					{
+						$crud->where('lid',NULL);
+					}
+					else 
+					{
+						$this->load->model('location');
+						$lid = $this->location->get_location_id($this->lname);
+						
+						$crud->where('lid',$lid);
+					}
+				}
+				
+				if(isset($this->type_id) && !empty($this->type_id)) 
+					$crud->where('type_id',$this->type_id);
+					//$crud->where('type_id',1);
 				$crud->unset_add();
 			
 				//if(get_priviledge_level() != 'admin')
@@ -79,13 +100,16 @@ class Items_location extends CI_Controller {
 				*/
 				//$crud->columns('name','description');
 	 			//$crud->callback_add_field('name',array($this,'add_name_callback'));
-				
+	            $crud->callback_column($this->unique_field_name('lid'),array($this,'blankFormatting'));
+	            
 				$crud->set_relation('type_id','types','name');
 				$crud->set_relation('lid','location','name');
+	 			$crud->add_action('Assign Location', '', 'assign_location/from_gc','ui-icon-image');
+				
 	 			$output = $crud->render();
 	 			
 	 			$data['output']=$output;
-			}
+//			}
 			
 			//print_r($output);
 			//$output['location_list']=array(1,2,3);
@@ -101,16 +125,52 @@ class Items_location extends CI_Controller {
 		}
 		
 	}
+	function unique_field_name($field_name) 
+	{
+	    return 's'.substr(md5($field_name),0,8); //This s is because is better for a string to begin with a letter and not with a number
+    }
 	
 	function get_lname()
 	{
-		return $this->lname;
+		if(isset($this->lname) && !empty($this->lname))
+			return $this->lname;
+		else 
+			return 'ALL';
+	}
+	function get_type_name()
+	{
+		$this->load->model('types');
+		
+		$type_name= $this->types->get_name($this->type_id);
+		
+		if(isset($type_name) && !empty($type_name)) return  $type_name;
+		else return 'ALL';
+		
+	}
+	
+	function get_type_id()
+	{
+		return $this->type_id;
 	}
 	function get_locations()
 	{
 		$this->load->model('location');
-		return $this->location->get_locations();
+		
+		$location[''] = 'ALL';
+		$location['NO_LOCATION'] = 'NO LOCATION';
+		$temp = $this->location->get_locations();
+		return array_merge($location,$temp);
+		
 	}
+	function get_types()
+	{
+		
+		$this->load->model('types');
+		$types= $this->types->get_id_name_tuples();
+		$types['']='ALL';  
+		return $types; 
+	}
+	
 	function location_exists($location)
 	{
 		$this->load->model('location');
@@ -123,6 +183,14 @@ class Items_location extends CI_Controller {
 		{
 			return TRUE;
 		}
+	}
+	function blankFormatting($value, $row)
+	{
+		//echo "here";
+		if(!isset($value) || empty($value)) 
+			return 'NO LOCATION';
+		else 
+			return $value;	
 	}
 	/*function add_name_callback()
 	{
